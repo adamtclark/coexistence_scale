80e594a08f65094d705908b59d5a1164c44c6518
atclark <adam.tclark@gmail.com>
2017-12-05 10:44:53 +0100

    Added function for iterating on HPC

---
+++ levins_metapopulation/iterate_spatial_scale.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+error
+rm(list=ls())
+#setwd("~/Dropbox/Projects/032_Coexistence_mechanisms/src/levins_metapopulation/")
+
+niterations<-2
+scalelst<-c(0.01, 0.02, 0.05, 0.1, 0.3, 0.5, 1)
+
+#matrices for output
+
+
+
+
+
+
+
+
+
+
+
+
+#save output (repeat for every scale)
