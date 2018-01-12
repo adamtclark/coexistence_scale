646231ce76b4f4dfccd207deda8178748cdb5732
atclark <adam.tclark@gmail.com>
2018-01-12 11:48:48 +0100

    Updated to make figures for example models; eig/r0; CV/beta; and map

--- levins_metapopulation/FIGURE_plot_example_timeseries.R
+++ levins_metapopulation/FIGURE_plot_example_timeseries.R
@@ -18,7 +18,7 @@ xfac<-3
 xfac_fast<-7
 ptb<-0.33
 collst<-adjustcolor(c("grey51", brewer.pal(4, "Set1")), alpha.f = 0.7)
-collst2<-adjustcolor(c("blue", "red", "black"), alpha.f = 0.7)
+collst2<-adjustcolor(brewer.pal(3, "Dark2"), alpha.f = 0.7)
 
 #simulation lengths
 tinit<-300
@@ -53,7 +53,6 @@ E_meta<-getEmeta$Eout
 E_meta_tot<-getEmeta$Eout_tot
   
 invar_meta<-estimate_invar(out = out_meta_long, E=E_meta, burnin=0, doplot=FALSE, Etot = E_meta_tot, niter = 0)
-  
 beta_meta<-beta_estimate(out=out_meta, outlng = out_meta_long, Emat = E_meta, eigout = eig_meta2, r0out = r0_meta, burnin = 100)
 
 ##### Hubbell neutal model
@@ -73,18 +72,15 @@ eig_neut2<-estimate_eqreturn(out_neut, simtime=tsim, runtype="neutral", replace_
 
 r0_neut<-estimate_rarereturn(out = eig_neut1$out_lst0, simtime=tsim, burnin=tsim, runtype="neutral", doplot = FALSE)
 
+set.seed(171206)
+out_neut_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_neut, talktime = 0, runtype = "neutral")
+getEneut<-getE(out_neut_long, Elst = 2:10)
+E_neut<-getEneut$Eout
+E_neut_tot<-getEneut$Eout_tot
 
-if(FALSE) {
-  set.seed(171206)
-  out_neut_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_neut, talktime = 0, runtype = "neutral")
-  getEneut<-getE(out_neut_long, Elst = 2:10)
-  E_neut<-getEneut$Eout
-  E_neut_tot<-getEneut$Eout_tot
-  
-  invar_neut<-estimate_invar(out_neut_long, E=E_neut, burnin=0, doplot=FALSE, Etot = E_neut_tot, niter = 0)
-  
-  beta_neut<-beta_estimate(out=out_neut, outlng = out_neut_long, Emat = E_neut, eigout = eig_neut2, r0out = r0_neut, burnin = 100)
-}
+invar_neut<-estimate_invar(out_neut_long, E=E_neut, burnin=0, doplot=FALSE, Etot = E_neut_tot, niter = 0)
+
+beta_neut<-beta_estimate(out=out_neut, outlng = out_neut_long, Emat = E_neut, eigout = eig_neut2, r0out = r0_neut, burnin = 100)
 
 ##### Disturbance model
 clst_dist= c(0.145, 0.2)*xfac
@@ -105,19 +101,15 @@ eig_dist2<-estimate_eqreturn(out_dist_0, simtime=tsim, runtype="disturbance", re
 out_dist_0<-rerunrun_metapopulation(out=eig_dist1$out_lst0, tmax=0, talktime = 0, runtype = "metapopulation", perturb = distlst, replace_perturb = 0)
 r0_dist<-estimate_rarereturn(out_dist_0, simtime=tsim, burnin=tsim, runtype="disturbance", doplot = FALSE, prt = distlst,  prtfrq = prtfrq)
 
-if(FALSE) {
-  set.seed(171217)
-  out_dist_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_dist, talktime = 0, runtype = "disturbance", prt = distlst,  prtfrq = prtfrq)
-  getEdist<-getE(out_dist_long, Elst = 2:10)
-  E_dist<-getEdist$Eout
-  E_dist_tot<-getEdist$Eout_tot
-  
-  invar_dist<-estimate_invar(out_dist_long, E=E_dist, burnin=100, doplot=FALSE, Etot = E_dist_tot, niter = 0)
-  
-  out_dist_nodist<-run_metapopulation(tmax=200, gridout = gridout, population = population_dist, talktime = 0, runtype = "metapopulation")
-  
-  beta_dist<-beta_estimate(out=out_dist, outlng = out_dist_long, Emat = E_dist, eigout = eig_dist2, r0out = r0_dist, burnin = 100)
-}
+set.seed(171217)
+out_dist_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_dist, talktime = 0, runtype = "disturbance", prt = distlst,  prtfrq = prtfrq)
+getEdist<-getE(out_dist_long, Elst = 2:10)
+E_dist<-getEdist$Eout
+E_dist_tot<-getEdist$Eout_tot
+
+invar_dist<-estimate_invar(out_dist_long, E=E_dist, burnin=100, doplot=FALSE, Etot = E_dist_tot, niter = 0)
+out_dist_nodist<-run_metapopulation(tmax=200, gridout = gridout, population = population_dist, talktime = 0, runtype = "metapopulation")
+beta_dist<-beta_estimate(out=out_dist, outlng = out_dist_long, Emat = E_dist, eigout = eig_dist2, r0out = r0_dist, burnin = 100)
 
 ##### Try psf model
 clst_psf= c(1.5, 1)*xfac
@@ -133,17 +125,13 @@ eig_psf2<-estimate_eqreturn(out_psf, simtime=tsim, runtype="psf", replace_pertur
 
 r0_psf<-estimate_rarereturn(eig_psf1$out_lst0, simtime=tsim, burnin=tsim, runtype="psf", doplot = FALSE)
 
-if(FALSE) {
-  set.seed(180108)
-  out_psf_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_psf, talktime = 0, runtype = "psf")
-  getEpsf<-getE(out_psf_long, Elst = 2:10)
-  E_psf<-getEpsf$Eout
-  E_psf_tot<-getEpsf$Eout_tot
-  
-  invar_psf<-estimate_invar(out_psf_long, E=E_psf, burnin=100, doplot=FALSE, Etot = E_psf_tot, niter = 0)
-  
-  beta_psf<-beta_estimate(out=out_psf, outlng = out_psf_long, Emat = E_psf, eigout = eig_psf2, r0out = r0_psf, burnin = 10)
-}
+set.seed(180108)
+out_psf_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_psf, talktime = 0, runtype = "psf")
+getEpsf<-getE(out_psf_long, Elst = 2:10)
+E_psf<-getEpsf$Eout
+E_psf_tot<-getEpsf$Eout_tot
+invar_psf<-estimate_invar(out_psf_long, E=E_psf, burnin=100, doplot=FALSE, Etot = E_psf_tot, niter = 0)
+beta_psf<-beta_estimate(out=out_psf, outlng = out_psf_long, Emat = E_psf, eigout = eig_psf2, r0out = r0_psf, burnin = 10)
 
 ##### Try rps model
 intmat_rps<-rbind(c(1,0,0,1),
@@ -164,17 +152,15 @@ eig_rps2<-estimate_eqreturn(out_rps, simtime=tsim, runtype="rps", replace_pertur
 
 r0_rps<-estimate_rarereturn(out = eig_rps1$out_lst0, simtime=tsim, burnin=tsim, runtype="rps", doplot = FALSE)
 
-if(FALSE) {
-  set.seed(180104)
-  out_rps_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_rps, talktime = 0, runtype = "rps", compmat = intmat_rps)
-  getErps<-getE(out_rps_long, Elst = 2:10)
-  E_rps<-getErps$Eout
-  E_rps_tot<-getErps$Eout_tot
-  
-  invar_rps<-estimate_invar(out_rps_long, E=E_rps, burnin=0, doplot=FALSE, Etot = E_rps_tot, niter = 0)
-  
-  beta_rps<-beta_estimate(out=out_rps, outlng = out_rps_long, Emat = E_rps, eigout = eig_rps2, r0out = r0_rps, burnin = 10)
-}
+set.seed(180104)
+out_rps_long<-run_metapopulation(tmax=1000, gridout = gridout, population = population_rps, talktime = 0, runtype = "rps", compmat = intmat_rps)
+getErps<-getE(out_rps_long, Elst = 2:10)
+E_rps<-getErps$Eout
+E_rps_tot<-getErps$Eout_tot
+
+invar_rps<-estimate_invar(out_rps_long, E=E_rps, burnin=0, doplot=FALSE, Etot = E_rps_tot, niter = 0)
+
+beta_rps<-beta_estimate(out=out_rps, outlng = out_rps_long, Emat = E_rps, eigout = eig_rps2, r0out = r0_rps, burnin = 10)
 
 ############################################################
 # Plot examples of each model
@@ -296,7 +282,7 @@ dev.off()
 
 
 ############################################################
-# Plot examples of eigen, invasion, CV, and beta
+# Plot examples of eigen and invasion
 ############################################################
 #out<-out_meta; eigout<-eig_meta2; r0out<-r0_meta; collst<-collst; burnin=200; burnine=100; dburnin=100; plotpos=1; doceq=0
 
@@ -460,11 +446,128 @@ pdf("figures/FIGURE_eig_r0_examples.pdf", width=5, height=6, colormodel = "cmyk"
 tmp<-statsplotfun(out=out_meta, eigout=eig_meta2, r0out=r0_meta, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 2)
 dev.off()
 
-
-
+pdf("figures/SUP_FIGURE_eig_r0_examples_allmodels.pdf", width=5, height=6, colormodel = "cmyk")
+tmp<-statsplotfun(out=out_meta, eigout=eig_meta2, r0out=r0_meta, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 2)
 tmp<-statsplotfun(out=out_dist, eigout=eig_dist2, r0out=r0_dist, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 1)  
 tmp<-statsplotfun(out=out_psf, eigout=eig_psf2, r0out=r0_psf, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 1)  
 tmp<-statsplotfun(out=out_rps, eigout=eig_rps2, r0out=r0_rps, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 1)  
 tmp<-statsplotfun(out=out_neut, eigout=eig_neut2, r0out=r0_neut, collst=collst, burnin=200, burnine=100, dburnin=100, plotpos=1, doceq = 1)  
+dev.off()
 
+############################################################
+# Plot examples of CV and beta
+############################################################
+outlong<-out_meta_long; invarout<-invar_meta; betaout<-beta_meta; collst<-collst; collst2<-collst2; burnin=200; plotpos=1; doceq=2; cleanupbeta<-2
+
+CVplotfun<-function(outlong, invarout, betaout, collst, collst2, burnin=0, plotpos=1, doceq=0, cleanupbeta=FALSE, ...) {
+  #Plotting environment
+  m<-rbind(c(1,1,1,3,3),
+           c(2,2,2,3,3))
+  layout(m)
+  par(mar=c(4,5,2,1), oma=c(0,0,0,0))
+  
+  ofs2<-c(0.19, -0.06)
+  ofs3<-c(0.29, -0.0225)
+  
+  ##### Invar
+  #plot time series
+  invarlst<-outlong$output[-c(1:burnin),plotpos+1]/outlong$plotdata$ngrid
+  matplot(outlong$output[-c(1:burnin),1], invarlst, type="l", lty=1, col=collst[-1], lwd=1.5, xlab="", ylab="", xaxs="i",
+          ylim=range(invarlst)*c(0.98, 1), axes=FALSE, ...)
+  axis(1); axis(2, las=2); box()
+  if(sum(doceq)==2) {
+    abline(h=c(sum(outlong$plotdata$ceq), outlong$plotdata$ceq), lty=3, col=collst, lwd=1)
+  } else if(sum(doceq)==1) {
+    abline(h=sum(outlong$plotdata$ceq), lty=3, col=collst, lwd=1)
+  }
+  mtext("simulation time", 1, line=2.8, cex=1.1)
+  mtext("relative abundance", 2, line=3.4, cex=1.1)
+  put.fig.letter("a.", "topleft", offset=ofs2, cex=1.6)
+  
+  tmp<-outlong$output[,2]/outlong$plotdata$ngrid
+  tmptm<-outlong$output[,1]
+  
+  startlst<-(nrow(outlong$output)-burnin)*c(0.2, 0.3, 0.5, 0.7, 0.8)+burnin
+  
+  y11<-min(tmp[tmptm>startlst[1] & tmptm<startlst[2]]); y12<-max(tmp[tmptm>startlst[1] & tmptm<startlst[2]])
+  y21<-min(tmp[tmptm>startlst[4] & tmptm<startlst[5]]); y22<-max(tmp[tmptm>startlst[4] & tmptm<startlst[5]])
+  segments(c(startlst[1], startlst[1], startlst[2], startlst[2]), c(y11, y12, y12, y11), c(startlst[1], startlst[2], startlst[2], startlst[1]), c(y12, y12, y11, y11), lwd=2)
+  segments(c(startlst[4], startlst[4], startlst[5], startlst[5]), c(y22, y21, y21, y22), c(startlst[4], startlst[5], startlst[5], startlst[4]), c(y21, y21, y22, y22), lwd=2)
+  
+  arrows(startlst[3], (y11+y21)/2, startlst[4], y21, lwd=2, length = 0.1, lend=2)
+  arrows(startlst[3], (y11+y21)/2, startlst[2], y11, lwd=2, length = 0.1, lend=2)
+  
+  text(startlst[3], (y11+y21)/2, pos=1, labels = "time lag")
+  
+  text(mean(startlst[1:2]), y11, pos=1, labels = "training set")
+  text(mean(startlst[4:5]), y21, pos=1, labels = "testing set")
+  
+  
+  #plot CV
+  plot(invarout$pdlag_list[[1]]$laglst, invarout$pdlag_list[[plotpos]]$CVest[,plotpos], xlab="", ylab="", type="l", lty=1, lwd=1.5, col=collst[2], xaxs="i", axes=FALSE, ...); abline(h=0, lty=3)
+  axis(1); axis(2, las=2); box()
+  
+  mtext("time lag", 1, line=2.8, cex=1.1)
+  mtext(expression(italic("CV")), 2, line=3.4, cex=1.1)
+  put.fig.letter("b.", "topleft", offset=ofs2, cex=1.6)
+  
+  
+  ##### Beta
+  betalst<-rbind(cbind(rowMeans(betaout$beta_eig), rowMeans(betaout$beta_r0), rowMeans(betaout$beta_0)), rep(NA, 3))
+  tlst<-0:(nrow(betaout$beta_eig))
+  beta_est<-matrix(nrow=nrow(betalst), ncol=ncol(betalst))
+  
+  if(cleanupbeta>0) {
+    for(i in 1:ncol(beta_est)) {
+      beta_est[,i]<-exp(predict(loess(log(betalst[,i])~tlst, enp.target = cleanupbeta), newdata=data.frame(tlst=tlst)))
+    }
+  } else {
+    beta_est<-betalst
+  }
+  
+  hlst<-c(1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001)
+  matplot(tlst, beta_est, type="n", lty=1, col=collst2, lwd=1.5, xlab="", ylab="", xaxs="i", log="y", axes=F, ...)
+  abline(h=hlst, col=adjustcolor(1, alpha.f = 0.3), lty=1, lwd=1)
+  matlines(tlst, beta_est, lty=1, col=collst2, lwd=1.5)
+  
+  mtext("time since event", 1, line=2.8, cex=1.1)
+  mtext(expression(paste("community dissimilarity")), 2, line=3, cex=1.1)
+  axis(1); axis(2, las=2); box()
+  put.fig.letter("c.", "topleft", offset=ofs3, cex=1.6)
+  
+  return(invarlst)
+}
+
+pdf("figures/FIGURE_CV_examples.pdf", width=6, height=4, colormodel = "cmyk")
+tmp<-CVplotfun(outlong=out_meta_long, invarout=invar_meta, betaout=beta_meta, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+dev.off()
+
+pdf("figures/SUP_FIGURE_CV_examples_allmodels.pdf", width=6, height=4, colormodel = "cmyk")
+tmp<-CVplotfun(outlong=out_meta_long, invarout=invar_meta, betaout=beta_meta, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+tmp<-CVplotfun(outlong=out_neut_long, invarout=invar_neut, betaout=beta_neut, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+tmp<-CVplotfun(outlong=out_dist_long, invarout=invar_dist, betaout=beta_dist, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+tmp<-CVplotfun(outlong=out_psf_long, invarout=invar_psf, betaout=beta_psf, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+tmp<-CVplotfun(outlong=out_rps_long, invarout=invar_rps, betaout=beta_rps, collst=collst, collst2=collst2, burnin=200, plotpos=1, doceq=2, cleanupbeta=20)
+dev.off()
+
+
+############################################################
+# Plot example of map
+############################################################
+grid_sub<-grid_subset(gridout, size = 0.01)
+grid_sub2<-grid_subset(gridout, size = 0.5)
+
+pdf("figures/FIGURE_spatialsubset_map.pdf", width=5, height=5, colormodel = "cmyk")
+par(mar=c(2,2,2,2), oma=c(2,2,0,0))
+plot_map(out_meta, gridout = gridout, grid_sub = grid_sub, collst=collst[-1])
+
+segments(grid_sub2$borders[c(1,1,2,2)]+0.5*c(-1,-1,1,1), grid_sub2$borders[c(3,4,4,3)]+0.5*c(-1,1,1,-1), grid_sub2$borders[c(1,2,2,1)]+0.5*c(-1,1,1,-1), grid_sub2$borders[c(4,4,3,3)]+0.5*c(1,1,-1,-1), col="black", lwd=4)
+segments(grid_sub2$borders[c(1,1,2,2)]+0.5*c(-1,-1,1,1), grid_sub2$borders[c(3,4,4,3)]+0.5*c(-1,1,1,-1), grid_sub2$borders[c(1,2,2,1)]+0.5*c(-1,1,1,-1), grid_sub2$borders[c(4,4,3,3)]+0.5*c(1,1,-1,-1), col="white", lwd=1)
+
+shadowtext(51, 59, "1%", cex=1.3)
+shadowtext(51, 90, "50%", cex=1.3)
+
+mtext("x position", 1, line=2.3, cex=1.1)
+mtext("y position", 2, line=2.3, cex=1.1)
+dev.off()
 
