8d1f257859dd26061ad2adb2ac0d6478f75b9e79
adamtclark <adam.tclark@gmail.com>
2018-12-30 16:21:52 +0100

    Updated to show fractions too

--- levins_metapopulation/plot_demstoch_vs_scale.R
+++ levins_metapopulation/plot_demstoch_vs_scale.R
@@ -7,7 +7,7 @@ setwd("~/Dropbox/Projects/032_Coexistence_mechanisms/src/levins_metapopulation/"
 source("run_metapopulation_wrapper.R")
 
 #set up for runs
-niterations<-10   #CHANGE TO ALTER NUMBER OF ITERATIONS
+niterations<-100   #CHANGE TO ALTER NUMBER OF ITERATIONS
 scalelst<-c(0.005, 0.01, 0.05, 0.1, 0.5, 0.75, 1)
 
 radlst<-Inf
@@ -27,32 +27,41 @@ population_meta<-populate(gridout, nlst = floor(getceq(clst_meta, mlst_meta)*pro
 varmat1<-matrix(nrow=niterations, ncol=length(scalelst))
 varmat2<-matrix(nrow=niterations, ncol=length(scalelst))
 
-for(i in 1:niterations) {
-  out<-run_metapopulation(tmax=tmax, nsteps = tmax, gridout, population_meta, talktime = 0, runtype="metapopulation")
-  for(j in 1:length(scalelst)) {
-    grid_sub<-grid_subset(gridout, size = scalelst[j])
-    out2<-rerunrun_metapopulation(out=out, tmax=50, runtype="metapopulation", sites_sub = grid_sub$sites, talktime = 0)
-    
-    tmp<-apply(out2$output_spatial[-c(1:burnin),-1], 2, function(x) sd(x)/mean(x))
-    
-    varmat1[i,j]<-tmp[1]
-    varmat2[i,j]<-tmp[2]
-  }
-  print(i/niterations)
-}  
+if(FALSE) {
+  for(i in 1:niterations) {
+    out<-run_metapopulation(tmax=tmax, nsteps = tmax, gridout, population_meta, talktime = 0, runtype="metapopulation")
+    for(j in 1:length(scalelst)) {
+      grid_sub<-grid_subset(gridout, size = scalelst[j])
+      out2<-rerunrun_metapopulation(out=out, tmax=50, runtype="metapopulation", sites_sub = grid_sub$sites, talktime = 0)
+      
+      tmp<-apply(out2$output_spatial[,-1], 2, function(x) sd(x)/mean(x))
+      
+      varmat1[i,j]<-tmp[1]
+      varmat2[i,j]<-tmp[2]
+    }
+    print(i/niterations)
+  }  
   
+  save(list = c("varmat1", "varmat2"), file = "output/demstochvar.rda")
+} else {
+  load("output/demstochvar.rda")
+}
+
 qtmat1<-t(apply(varmat1, 2, function(x) quantile(x, pnorm(-1:1), na.rm=T)))
 qtmat2<-t(apply(varmat2, 2, function(x) quantile(x, pnorm(-1:1), na.rm=T)))
 
 pdf("figures/SUP_FIGURE_demstoch_vs_scale.pdf", colormodel = "cmyk", width=5, height=4)
-  par(mar=c(4,4,2,2))
-  matplot(scalelst*(100^2), qtmat1, lty=c(2,1,2), lwd=2, type="l", col=1, xlab="Grid Size", ylab="CV(N)", ylim=c(0, 0.25), axes=F, log="x")
+  par(mar=c(4,4,4,2))
+  matplot(scalelst*(100^2), qtmat1, lty=c(2,1,2), lwd=2, type="l", col=1, xlab="grid size", ylab="CV(N)", ylim=c(0, 0.25), axes=F, log="x")
   matlines(scalelst*(100^2), qtmat2, lty=c(2,1,2), lwd=2, col=2)
   legend("topright", c("Species 1", "Species 2"), col=c(1,2), lty=1, lwd=2, bty="n")
   
   axis(1, las=2)
   axis(2, las=2)
+  axis(3, las=2, at=scalelst*(100^2), labels = scalelst)
+  mtext("spatial extent, fraction of maximum", 3, line=3)
   box()
+  abline(v=scalelst*(100^2), lty=3)
 dev.off()  
 
 
